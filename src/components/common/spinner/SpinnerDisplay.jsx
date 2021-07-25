c14059aac50de71d10af1aa5dad649256e445a19
ayoub <ayoub.belghar@gmail.com>
2021-07-25 13:49:52 +0100

    change spinner

--- src/components/common/spinner/SpinnerDisplay.jsx
+++ src/components/common/spinner/SpinnerDisplay.jsx
@@ -1,8 +1,4 @@
 import React from 'react'
 import { Spinner } from 'react-bootstrap'
 
-export const SpinnerDisplay = () => (
-  <Spinner animation="border" role="status">
-    <span className="visually-hidden">Loading...</span>
-  </Spinner>
-)
+export const SpinnerDisplay = () => <Spinner animation="grow" />
