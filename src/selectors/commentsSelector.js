6841f0762cec9598c2f29e0d163c96b0709cc5c4
ayoub <ayoub.belghar@gmail.com>
2021-07-26 11:14:44 +0100

    remove old comment from comments selector

--- src/selectors/commentsSelector.js
+++ src/selectors/commentsSelector.js
@@ -1,7 +1,6 @@
 import { createSelector } from "reselect";
 
 export const commentsSelector = createSelector(
- // (state, id) => state.get('comments').toJS().find(comment => Object.keys(comment)[0] == id),
   state => state.get('comments'),
   comments => comments
 )
