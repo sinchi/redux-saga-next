1f7560e21814706fbcefd7ff1197b259f7c318fa
ayoub <ayoub.belghar@gmail.com>
2021-07-26 12:04:14 +0100

    refactor comments selector

--- src/components/posts/postItem/PostItemContainer.js
+++ src/components/posts/postItem/PostItemContainer.js
@@ -5,15 +5,13 @@ import { PostItemDisplay } from './PostItemDisplay'
 
 const mapStateToProps = (state, {id}) => {
   const posts = postsSelector(state);
-  const comments = commentsSelector(state);
-  const postComments = comments.find((comments, index) => index == id)
+  const comments = commentsSelector(state, id);
   const post = posts.find(post => post.get('id') === id)
-
   return {
     fetched: post !== undefined,
-    postCommentsFetched: postComments !== undefined,
+    postCommentsFetched: comments !== undefined,
     ...post.toJS(),
-    postComments: postComments ? Object.values(postComments)[0] : null,
+    comments,
     postId: id
   }
 }
