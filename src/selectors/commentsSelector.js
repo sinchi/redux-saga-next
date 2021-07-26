d034285675eb43286a6ad89d5ec0645ad382d6c8
ayoub <ayoub.belghar@gmail.com>
2021-07-26 11:12:48 +0100

    refactor comments post selector, view and reducer

--- src/selectors/commentsSelector.js
+++ src/selectors/commentsSelector.js
@@ -1,6 +1,7 @@
 import { createSelector } from "reselect";
 
 export const commentsSelector = createSelector(
-  (state, id) => state.get('comments').toJS().find(comment => Object.keys(comment)[0] == id),
+ // (state, id) => state.get('comments').toJS().find(comment => Object.keys(comment)[0] == id),
+  state => state.get('comments'),
   comments => comments
 )
