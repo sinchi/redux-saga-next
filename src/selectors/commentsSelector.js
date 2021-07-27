430bd3687dac95a33884769a40e9d80d17190e6c
ayoub belghar <ayoub.belghar@gmail.com>
2021-07-27 09:23:10 +0200

    Refactor comments selectors

--- src/selectors/commentsSelector.js
+++ src/selectors/commentsSelector.js
@@ -2,5 +2,5 @@ import { createSelector } from "reselect";
 
 export const commentsSelector = createSelector(
   (state, postId) => state.get('comments').find(comment => comment[postId]),
-  comments => comments ? Object.values(comments)[0] : undefined
+  comments => comments ? ...Object.values(comments) : undefined
 )
